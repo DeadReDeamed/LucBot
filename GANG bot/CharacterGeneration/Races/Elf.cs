bc569721863bd4af09fd3deeaf4da33bc1552f48
Luca <Luca.1999@hotmail.com>
2021-06-11 22:55:52 +0200

    Added Humanoid and Elf class

---
+++ GANG bot/CharacterGeneration/Races/Elf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GANG_bot.CharacterGeneration.Races
+{
+    class Elf : Humanoid
+    {
+        public override Stats bonusStats { get => new Stats() { Dexterity = 2 };}
+
+        public Elf(Stats stats) : base(stats)
+        {
+                
+        }
+    }
+}
---
+++ GANG bot/CharacterGeneration/Races/Humanoid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GANG_bot.CharacterGeneration.Races
+{
+    class Stats
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+
+        public Stats()
+        {
+
+        }
+
+    }
+
+    abstract class Humanoid
+    {
+        public abstract Stats bonusStats { get; }
+        public Stats stats { get; private set; }
+
+        public Humanoid(Stats stats)
+        {
+            this.stats = stats;
+        }
+    }
+}
